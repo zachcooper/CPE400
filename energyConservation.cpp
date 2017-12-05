08f57b2a6f27dd5dd5c538721af024a1b3e59fef
Roniel Padua <roniel@nevada.unr.edu>
2017-12-04 22:31:59 -0800

    Added some comments to energyConservation.cpp

--- energyConservation.cpp
+++ energyConservation.cpp
@@ -91,6 +91,8 @@ int maxPower(int energyRemaining[], bool sptSet[])
 {
 	int max = 0, max_index;
 
+	// loop through the vertices and if the remaining energy on a vertex
+	// is higher than the current max, then save it to the max variable
 	for(int i = 0; i < 20; i++)
 	{
 		if(sptSet[i] == false && energyRemaining[i] >= max)
@@ -98,6 +100,8 @@ int maxPower(int energyRemaining[], bool sptSet[])
 			max = energyRemaining[i], max_index = i;
 		}
 	}
+	// return the index of the vertex of the node with the highest
+	// energy remaining
 	return max_index;
 }
 
@@ -118,9 +122,15 @@ int printSoln(int energyRemaining[], int n)
 // find the solution for the packets route of least energy consumption from source to dest
 void findLowestEnergyPath(WeightedGraph::Vertex vertices[], Packet packet)
 {
+	/* initialize array to track the energy remaining for the nodes and a bool
+	array to keep track of whether or not one has already been visited to
+	avoid infinite loops*/
 	int energyRemaining[20];
 	bool sptSet[20];
 
+	/* iterate through and initialize energy remaining with current energy existing
+	in nodes currently and initialize visited bool array to false since no
+	traversals have been made yet*/
 	for(int i = 0; i < 20; i++)
 	{
 		energyRemaining[i] = vertices[i].getEnergy();
@@ -131,6 +141,8 @@ void findLowestEnergyPath(WeightedGraph::Vertex vertices[], Packet packet)
 
 	int j = 0, u = -1;
 
+	/* for each vertex, loop through the vertices and find the adjacent vertex
+	with the highest energy remaining */
 	while(j < 19 && u != packet.dest)
 	{
 		u = maxPower(energyRemaining, sptSet);
@@ -143,32 +155,45 @@ void findLowestEnergyPath(WeightedGraph::Vertex vertices[], Packet packet)
 		j++;
 	}
 
+	// print the energy remaining in each node and the system overall
 	printSoln(energyRemaining, 20);
 }
 
+//find the solution for the minimum distance from a node's immediate connections to other nodes
 int minDistance(WeightedGraph &net, bool sptSet[], int loc)
 {
+	// initialize temporary min to keep track of current min and weight to store weight
+	// value between two vertices
 	int min = 1000, min_index, weight;
 
+	// loop through each vertex
 	for(int i = 0; i < 20; i++)
 	{
-
+		// get the weight from the passed vertex to the iterated one in the network
 		net.getEdgeWeight(to_string(loc), to_string(i), weight);
+		// if the node has not been visited and the weight cost is
+		//smaller than the current minimum, then save the weight
 		if(sptSet[i] == false && weight <= min)
 		{
 			min = weight, min_index = i;
 		}
 	}
+	// return the vertex of the shortest connection to the local vertex
 	return min_index;
 }
 
+//find the solution for the shortest path for a packet to traverse through the network using its source and destination values
 void dijkstra(WeightedGraph::Vertex vertices[], WeightedGraph &net, Packet packet)
 {
 
+	/* arrays initialized to keep track of energy remaining in each node, as well as the
+	distance from one node to another, and a bool array to keep track of if a vertex has
+	been visited */
 	int energyRemaining[20];
 	int dist[20];
 	bool sptSet[20];
 
+	// loop through and initialize
 	for(int i = 0; i < 20; i++)
 	{
 		dist[i] = 1000;
@@ -176,12 +201,16 @@ void dijkstra(WeightedGraph::Vertex vertices[], WeightedGraph &net, Packet packe
 		sptSet[i] = false;
 	}
 
+	// distance from packet source to itself is 0
 	dist[packet.source] = 0;
 
 	//energyRemaining[packet.source] = 0;
 
 	int j = 0, u = -1;
 
+	/* look through and find the shortest path until the packet reaches the
+	destination. subtract the energy consumption of the packet from each visited
+	vertex */
 	while(j < 19 && u != packet.dest)
 	{
 		u = minDistance(net, sptSet, j);
@@ -194,5 +223,6 @@ void dijkstra(WeightedGraph::Vertex vertices[], WeightedGraph &net, Packet packe
 		j++;
 	}
 
+	//print the energy remaining in the nodes and the network overall
 	printSoln(energyRemaining, 20);
 }
